f92d2186fc374c7f107dcc4db0058a16fd3433ce
Okrekso <semikoseker@gmail.com>
2018-11-14 00:35:47 +0200

    host fix

--- js/sql_definition.js
+++ js/sql_definition.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 // requiers
 
-let host = '185.224.137.3';
+let host = process.env.host;
 let user = process.env.DB_User;
 let pass = process.env.DB_Pass;
 let database = 'u380338396_itb';
