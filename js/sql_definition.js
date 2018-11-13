418b786dfa6f3f40e3658c1498ed55a31998abbe
Okrekso <semikoseker@gmail.com>
2018-11-14 00:34:01 +0200

    ip fix

--- js/sql_definition.js
+++ js/sql_definition.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 // requiers
 
-let host = '185.224.137.110';
+let host = '185.224.137.3';
 let user = process.env.DB_User;
 let pass = process.env.DB_Pass;
 let database = 'u380338396_itb';
