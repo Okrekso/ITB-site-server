ed43db78c47018fd70fc7013673b23776623bf84
Okrekso <semikoseker@gmail.com>
2018-11-14 17:28:58 +0200

    secure fix

--- js/routers/r_payment.js
+++ js/routers/r_payment.js
@@ -1,4 +1,5 @@
 let router = require("express").Router();
+const secure = require("../database_operations/secureCode");
 
 router.post("/acceptPayment", (req, res) => {
   console.log(req.body.data, req.body.signature);
