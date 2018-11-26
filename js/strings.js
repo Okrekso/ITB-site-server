4959f75649a171d1d5104e8b38bd4cfbb17b9702
Okrekso <semikoseker@gmail.com>
2018-11-26 18:59:44 +0200

    site change to temporaly location

--- js/strings.js
+++ js/strings.js
@@ -1,6 +1,6 @@
 module.exports = {
 	s_accessForbitten: 'access forbidden!',
-	s_site: 'http://itbeans.online',
+	s_site: 'u380338396.hostingerapp.com',
 	s_userNotFound: 'user not found!',
 	s_notTime: 'too many requests, please wait!',
 	s_secureEncode: process.env.secureEncode
