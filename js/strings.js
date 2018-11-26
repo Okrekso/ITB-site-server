925a79dae17941d687d13352884e8639afc4516c
Okrekso <semikoseker@gmail.com>
2018-11-26 23:31:13 +0200

    site fix

--- js/strings.js
+++ js/strings.js
@@ -1,6 +1,6 @@
 module.exports = {
 	s_accessForbitten: 'access forbidden!',
-	s_site: 'http://u380338396.hostingerapp.com',
+	s_site: 'http://itbeans.xyz',
 	s_userNotFound: 'user not found!',
 	s_notTime: 'too many requests, please wait!',
 	s_secureEncode: process.env.secureEncode
