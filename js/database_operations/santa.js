86eff08f02938f1609da2182f027a33f76823288
Okrekso <semikoseker@gmail.com>
2018-12-24 01:45:42 +0200

    friend debug

--- js/database_operations/santa.js
+++ js/database_operations/santa.js
@@ -6,7 +6,7 @@ function becomeSanta(userID) {
   console.log("start creating new santa");
   users.getUsers(users => {
     let id = userID;
-    while (id == userID || id==undefined) {
+    while (id == userID || id == undefined) {
       id = users[Math.floor(Math.random() * users.length)].Id;
     }
     console.log(`new santa created for ${userID}`);
@@ -26,22 +26,32 @@ module.exports.getSantas = function(callback = santas => {}) {
 module.exports.findMySanta = function(userID, callback = res => {}) {
   userID = sqlstring.escape(userID);
   db.query(
-    `SELECT Name FROM USERS as u INNER JOIN e_santas as s ON s.FriendID=u.Id WHERE s.FriendID=${user.Id} AND Shown=1`,
+    `SELECT Name FROM USERS as u INNER JOIN e_santas as s ON s.FriendID=u.Id WHERE s.FriendID=${
+      user.Id
+    } AND Shown=1`,
     (result, err) => {
-      if(result[0]==undefined) callback("your santa is hidden :)");
+      if (result[0] == undefined) callback("your santa is hidden :)");
     }
   );
 };
 
 module.exports.findMyFriend = function(secureCode, callback = friend => {}) {
   console.log(secureCode, "finding santa");
-  users.findUserBySecure(secureCode, (user)=>{
-    if(user[0]==undefined) return;
-    userID = user['Id'];
-    db.query(`SELECT Name FROM USERS as u INNER JOIN e_santas as s ON s.FriendID=u.Id WHERE s.SantaID=${user.Id}`,(friend)=>{
-      console.log("friend:",friend);
-      if(friend[0]==undefined) return becomeSanta(userID);
-      callback(friend[0].Name);
-    });
+  users.findUserBySecure(secureCode, user => {
+    if (user[0] == undefined) {
+      console.log("no such user:",secureCode, user);
+      return;
+    }
+    userID = user["Id"];
+    db.query(
+      `SELECT Name FROM USERS as u INNER JOIN e_santas as s ON s.FriendID=u.Id WHERE s.SantaID=${
+        user.Id
+      }`,
+      friend => {
+        console.log("friend:", friend);
+        if (friend[0] == undefined) return becomeSanta(userID);
+        callback(friend[0].Name);
+      }
+    );
   });
 };
