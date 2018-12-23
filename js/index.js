7a2ccc1f16ae1793bceb4acf6c6636217584582a
Okrekso <semikoseker@gmail.com>
2018-12-23 20:48:18 +0200

    santa site added to origin

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ module.exports.isTest = false
 // strings
 var app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", strings.s_site);
+  res.header("Access-Control-Allow-Origin", strings.s_site.join(", "));
   res.header(
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Origin",
--- js/strings.js
+++ js/strings.js
@@ -1,6 +1,6 @@
 module.exports = {
 	s_accessForbitten: 'access forbidden!',
-	s_site: 'http://itbeans.xyz',
+	s_site: ['http://itbeans.xyz','http://santa.itbeans.xyz'],
 	s_userNotFound: 'user not found!',
 	s_notTime: 'too many requests, please wait!',
 	s_secureEncode: process.env.secureEncode
