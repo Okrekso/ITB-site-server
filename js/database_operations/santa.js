f8751dc3c6ebae5963bcefdc31a1f442a10c3d85
Okrekso <semikoseker@gmail.com>
2018-12-24 01:42:06 +0200

    fixes for santa choose

--- js/database_operations/santa.js
+++ js/database_operations/santa.js
@@ -3,10 +3,11 @@ const users = require("../database_operations/users");
 const sqlstring = require("sqlstring");
 
 function becomeSanta(userID) {
+  console.log("start creating new santa");
   users.getUsers(users => {
     let id = userID;
-    while (id == userID) {
-      id = users[Math.floor(Math.random() * users.length) + 0].ID;
+    while (id == userID || id==undefined) {
+      id = users[Math.floor(Math.random() * users.length)].Id;
     }
     console.log(`new santa created for ${userID}`);
 
@@ -34,7 +35,7 @@ module.exports.findMySanta = function(userID, callback = res => {}) {
 
 module.exports.findMyFriend = function(secureCode, callback = friend => {}) {
   console.log(secureCode, "finding santa");
-  users.findUserBySecure(secureCode,(user)=>{
+  users.findUserBySecure(secureCode, (user)=>{
     if(user[0]==undefined) return;
     userID = user['Id'];
     db.query(`SELECT Name FROM USERS as u INNER JOIN e_santas as s ON s.FriendID=u.Id WHERE s.SantaID=${user.Id}`,(friend)=>{
